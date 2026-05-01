0cf5ab3affa6d2ada2138ce4830bc4eb50694ba1
wayneleon1 <rurangwaleon10@gmail.com>
2026-05-01 11:48:55 +0200

    Build persistent layout shell: sidebar + header components

---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,19 @@
+import { Component, inject } from '@angular/core';
+import { BoardService } from '../../core/services/board.service';
+import { LayoutService } from '../../core/services/layout.service';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+})
+export class Header {
+  boardService = inject(BoardService);
+  layoutService = inject(LayoutService);
+
+  get hasColumns(): boolean {
+    return (this.boardService.activeBoard()?.columns.length ?? 0) > 0;
+  }
+}
---
+++ src/app/layout/sidebar/sidebar/sidebar.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { BoardService } from '../../../core/services/board.service';
+import { LayoutService } from '../../../core/services/layout.service';
+import { ThemeService } from '../../../core/services/theme.service';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+  boardService = inject(BoardService);
+  layoutService = inject(LayoutService);
+  themeService = inject(ThemeService);
+}
