f3a22a1c96fd11f9a9d358a0cd5ba4db226a5515
wayneleon1 <rurangwaleon10@gmail.com>
2026-05-04 11:46:32 +0200

    Add Modal Service and id utility

---
+++ src/app/core/services/modal.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, signal } from '@angular/core';
+
+export type ModalType =
+  | 'view-task'
+  | 'add-task'
+  | 'edit-task'
+  | 'delete-task'
+  | 'add-board'
+  | 'edit-board'
+  | 'delete-board'
+  | null;
+
+export interface ModalState {
+  type: ModalType;
+  taskId?: string;
+  boardId?: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class ModalService {
+  state = signal<ModalState>({ type: null });
+
+  open(type: Exclude<ModalType, null>, options?: { taskId?: string; boardId?: string }): void {
+    this.state.set({ type, ...(options ?? {}) });
+  }
+
+  close(): void {
+    this.state.set({ type: null });
+  }
+}
